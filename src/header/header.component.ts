f779456d835a74f88b8bf4f1cf1fa4c67124ff72
Dorisd97 <dorisd97@gmail.com>
2024-11-24 23:30:50 +0100

    Changes to frontend, logo added
    Logo is added and some fixes done

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css'] // Fixed typo: `styleUrl` to `styleUrls`
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
 
+  navigateHome(): void {
+    // Navigate to the home route and refresh its state
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate(['/']);
+    });
+  }
 }
